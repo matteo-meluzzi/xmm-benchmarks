7fe5c1bb2dc880d460672c57332129bb6ee172be
judomat <judomat@users.noreply.github.com>
2024-06-19 14:00:01 +0000

    updated tests

--- data-structures/stc/stc.c
+++ data-structures/stc/stc.c
@@ -53,7 +53,7 @@ int stack_try_push(struct stack *s, int data)
 /* Returns -1 if empty, -2 if lost */
 int stack_try_pop(struct stack *s, int *ret_data)
 {
-	struct stack_node *head = atomic_load_explicit(&s->head, memory_order_acquire);
+	struct stack_node *head = atomic_load_explicit(&s->head, memory_order_relaxed);
 
 	if (head == NULL)
 		return -1; /* empty */
@@ -61,7 +61,7 @@ int stack_try_pop(struct stack *s, int *ret_data)
 	*ret_data = atomic_load_explicit(&head->data, memory_order_relaxed);
 	struct stack_node *next = atomic_load_explicit(&head->next, memory_order_relaxed);
 	bool success = atomic_compare_exchange_strong_explicit(&s->head, &head, next,
-							       memory_order_release,
-							       memory_order_release);
+							       memory_order_relaxed,
+							       memory_order_relaxed);
 	return (success ? 0 : -2); /* CAVEAT: memory leak if successful */
 }
